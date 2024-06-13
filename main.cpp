2c8f09a9974c72eae72d1d388dd390abe3979eb1
eliann28 <quevedoelian5@gmail.com>
2024-06-12 21:39:59 -0300

    agrege el main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    int tipo_entero = 15 ; //Tipo de dato entero (EJ : 10, 20, 6 , sin puntos decimlales)
+
+    float tipo_flotante = 10.45; //datos decimales pero pequeños
+
+    double tipo_double = 16.34567834634;  // adtos decimales mas grandes
+
+    char letra = 'a' ; // un caracter.
+
+
+    cout << tipo_entero << endl ;
+    cout << tipo_flotante << endl ;
+    cout << tipo_double << endl ;
+    cout << letra << endl ; 
+
+
+
+
+
+    return 0 ;
+}
